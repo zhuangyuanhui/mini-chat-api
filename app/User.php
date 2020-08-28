36f8099d2c1ee28e6b69a8f438c4ae3a588451d2
zhuangyh <13580179149@163.com>
2020-08-28 09:54:00 +0800

    Z 修改user.php

--- app/User.php
+++ app/User.php
@@ -8,32 +8,18 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    public function aaa()
+    {
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    }
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function bbb()
+    {
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    }
+
+    public function ccc()
+    {
+
+    }
 }
