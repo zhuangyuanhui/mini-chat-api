22d0c9ae6e70de905968146fc63b4d947b148da5
13873459717 <2447511717@qq.com>
2020-08-28 10:29:22 +0800

    测试-test4

--- app/User.php
+++ app/User.php
@@ -8,5 +8,42 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'test' => 'test399999',
+    ];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts2 = [
+        'email_verified_at' => 'datetime',
+        'test' => 'test399999',
+    ];
 }
