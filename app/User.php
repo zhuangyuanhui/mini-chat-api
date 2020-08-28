86293db933248b5726e700989bc9ee9103c504c4
13873459717 <2447511717@qq.com>
2020-08-28 10:12:47 +0800

    测试-test3

--- app/User.php
+++ app/User.php
@@ -8,23 +8,5 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    public function aaa()
-    {
 
-    }
-
-    public function bbb()
-    {
-
-    }
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'test' => 'test399999',
-    ];
 }
