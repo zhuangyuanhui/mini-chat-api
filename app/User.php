bf6f7cd32f0ad226291cf2dd128f2ca91785c275
13873459717 <2447511717@qq.com>
2020-08-28 10:39:03 +0800

    测试-还原

--- app/User.php
+++ app/User.php
@@ -19,4 +19,21 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
